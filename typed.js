d3ffb0d6717aaea745bd27bb966786e99049309d
Florian de Radiguès <florian.radigues@gmail.com>
2023-10-20 17:55:53 +0200

    typed.js + display/css of it

---
+++ typed.js
@@ -0,0 +1,17 @@
+var typed = new Typed('#quote', {
+    strings: ["Ce n'est pas dans le monde extérieur que vous trouverez la paix.<br>Creusez au plus profond de vous et vous trouverez la perle.<br>-- Ma Anandamayî --<br><br>Le monde n'est rien d'autre qu' amour devenu matière.<br>-- Gitta Mallasz --"],
+    typeSpeed: 50,
+    showCursor: false,
+    loop: false,
+    backDelay: 3000,
+    backSpeed: 10,
+    
+});
+
+// var $secondType = new Typed('#author', {
+//     strings: ["Gitta Mallasz", "Ma Anandamayî"],
+//     typeSpeed: 50,
+//     showCursor: false,
+//     loop: true,
+
+// });
