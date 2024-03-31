d8afe70d0bf690a8510172e47bfd1004dee79210
Florian de Radiguès <florian.radigues@gmail.com>
2024-03-31 18:05:31 +0200

    banner responsivness

--- typed.js
+++ typed.js
@@ -1,6 +1,5 @@
 var typed = new Typed('#quote', {
     strings: ["Ce n'est pas dans le monde extérieur que vous trouverez la paix.<br>Creusez au plus profond de vous et vous trouverez la perle.<br>-- Ma Anandamayî --<br><br>Le monde n'est rien d'autre qu' amour devenu matière.<br>-- Gitta Mallasz --"],
     typeSpeed: 40,
-    showCursor: false,
-    
+    showCursor: false,    
 });
